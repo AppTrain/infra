69261afc9239d6eccf16ff33b8d8a697ec5be0a5
Jeremiah Campbell <jeremiah@integralthread.com>
2019-06-20 11:18:57 -0400

    add r53 qbs.accuenplatform.com as nice name for qubole bastion server

---
+++ aws/accuen/qubole/r53.tf
@@ -0,0 +1,12 @@
+
+data "aws_route53_zone" "root" {
+  name = "accuenplatform.com."
+}
+
+resource "aws_route53_record" "qubole_bastion"{
+    zone_id = "${data.aws_route53_zone.root.zone_id}"
+    name = "qbs.accuenplatform.com"
+    type = "A"
+    ttl = "30"
+    records = ["${aws_eip.bastion.public_ip}"]
+}
