c491b95ce1c250cb76e0b31daf73797912ee967e
Jeremiah Campbell <jeremiah@integralthread.com>
2019-06-25 17:33:17 -0400

    creates a repository with corresponding docker repo

--- aws/accuen/ops/repos.tf
+++ aws/accuen/ops/repos.tf
@@ -1,4 +1,18 @@
 
-module repos {
-    source = "../../../terraform-modules/repos"
+# module repos {
+#     source = "../../../terraform-modules/repos"
+# }
+
+module docker_python {
+    source = "../../../terraform-modules/repository"
+    name = "docker-python"
+    description = "docker python base image"
+    topics = ["python", "docker"]
+}
+
+module airflow {
+    source = "../../../terraform-modules/repository"
+    name = "airflow"
+    description = "airflow baked into docker"
+    topics = ["airflow", "python", "docker"]
 }
