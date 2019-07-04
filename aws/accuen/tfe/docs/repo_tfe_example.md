06d01f5d89be6720475e28bef27d89708bb3f705
Jeremiah Campbell <jeremiah@integralthread.com>
2019-07-03 23:13:38 -0400

    tfe experiment

---
+++ aws/accuen/tfe/docs/repo_tfe_example.md
@@ -0,0 +1,7 @@
+# tfe_example
+
+- **description**: accuen pipeline data
+- **topics**: annalect
+- [github.com/accuenmedia/tfe_example](https://github.com/accuenmedia/tfe_example)
+- **ssh clone**: `git@github.com:accuenmedia/tfe_example.git`
+- **ecr repo**: `348194362585.dkr.ecr.us-west-2.amazonaws.com/accuenmedia/tfe_example`
--- aws/accuen/tfe/main.tf
+++ aws/accuen/tfe/main.tf
@@ -1,11 +1,30 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
-    config = {
-        organization = "annalectualops"
-        workspaces {
-            name = "tst"
-        }
+    hostname     = "app.terraform.io"
+    organization = "annalectualops"
+    workspaces {
+      name = "tst"
     }
   }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  version = "2.15"
+}
+
+provider "github" {
+  organization = "accuenmedia"
+  version = "2.1"
+}
+
+variable env {
+  description = "environment name"
+  default     = "tfe"
+}
+
+module example_tfe {
+    source = "../../../terraform-modules/repository"
+    name = "tfe_example"
+    description = "accuen pipeline data"
 }
