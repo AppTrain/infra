f0ad6a5e092e2f955ac98d0a753fb03678c96e7f
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-06 16:30:01 -0400

    wip smp cloning

--- aws/accuen/ops/django/r53.tf
+++ aws/accuen/ops/django/r53.tf
@@ -1,7 +1,24 @@
-# resource "aws_route53_record" "internal_postgres" {
-#   zone_id = "${module.this_buildenv.internal_zone_id}"
-#   name    = "django.${var.env}.in.accuenplatform.com"
-#   type    = "CNAME"
-#   ttl     = 300
-#   records = ["${module.django_clone.this_db_instance_address}"]
-# }
+data "aws_route53_zone" "inside" {
+  name         = "build-ops.in.accuenplatform.com."
+  private_zone = true
+}
+
+data "aws_route53_zone" "outside" {
+  name         = "build-ops.out.accuenplatform.com."
+}
+
+resource "aws_route53_record" "internal_postgres" {
+  zone_id = "${data.aws_route53_zone.inside.id}"
+  name    = "djangoclone.build-ops.in.accuenplatform.com"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${module.django_clone.this_db_instance_address}"]
+}
+
+resource "aws_route53_record" "smp_db" {
+  zone_id = "${data.aws_route53_zone.outside.id}"
+  name    = "clonetest.build-ops.out.accuenplatform.com"
+  type    = "A"
+  ttl     = 300
+  records = ["${aws_instance.smp_db.public_ip}"]
+}
--- terraform-modules/buildenv/r53.tf
+++ terraform-modules/buildenv/r53.tf
@@ -10,6 +10,10 @@ resource "aws_route53_zone" "internal" {
   }
 }
 
+data "aws_route53_zone" "main" {
+  name = "accuenplatform.com."
+}
+
 resource "aws_route53_zone" "external" {
   name = "${var.env}.out.accuenplatform.com."
 
@@ -17,6 +21,20 @@ resource "aws_route53_zone" "external" {
     zone        = "${var.env}.out.accuenplatform.com"
     Environment = "${var.env}"
   }
+}
+
+resource "aws_route53_record" "external-ns" {
+  zone_id = "${data.aws_route53_zone.main.zone_id}"
+  name    = "${var.env}.out.accuenplatform.com"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    "${aws_route53_zone.external.name_servers.0}",
+    "${aws_route53_zone.external.name_servers.1}",
+    "${aws_route53_zone.external.name_servers.2}",
+    "${aws_route53_zone.external.name_servers.3}",
+  ]
 }
 
 resource "aws_route53_record" "bastion" {
