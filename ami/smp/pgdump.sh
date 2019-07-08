8c6d547af5046314bbcad79c9bc0b453e344f1fa
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-08 18:06:35 -0400

    wip smp clone

--- ami/smp/pgdump.sh
+++ ami/smp/pgdump.sh
@@ -8,4 +8,5 @@ psql -d $CLONE_DB -c "select now();"
 echo "*****************"
 echo "Database $PGHOST is ready"
 
-pg_dump -d $ClONE_DB /tmp/dump.sql
+mkdir -p /var/data
+pg_dump -d $ClONE_DB > /var/data/smp.sql
