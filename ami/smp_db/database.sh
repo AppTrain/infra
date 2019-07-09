81b6b1ee0403c9c52720a993623db7fc8e28ac7f
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-09 13:21:42 -0400

    attempt build of database

---
+++ ami/smp_db/database.sh
@@ -0,0 +1,10 @@
+set -e 
+
+echo "*****************"
+echo "$PACKER_BUILD_NAME -- SMP DJANGO CLONE"
+
+sudo -u postgres psql -q -f /var/data/smp.sql
+#sudo -u postgres pg_restore -e -O -x /var/data/smp.sql
+sudo -u postgres psql -q -c "select * from information_schema.schemata;"
+# echo "*****************"
+# echo "Database $PGDATABASE at $PGHOST is ready"
