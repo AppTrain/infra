e149e672ed2d9910f15b7fa5038b23198b3fad52
Jeremiah Campbell <jeremiah@integralthread.com>
2019-07-09 01:13:50 -0400

    fixing pgdump

--- ami/smp/pgdump.sh
+++ ami/smp/pgdump.sh
@@ -1,13 +1,14 @@
 set -e 
 
 echo "*****************"
-echo $PACKER_BUILD_NAME
+echo "$PACKER_BUILD_NAME -- SMP DJANGO CLONE"
 
-# pg_isready -d $CLONE_DB
-# psql -d $CLONE_DB -c "select now();"
-# echo "*****************"
-# echo "Database $CLONE_DB at $PGHOST is ready"
+dig $PGHOST
+pg_isready
+psql -c "select now();"
+echo "*****************"
+echo "Database $PGDATABASE at $PGHOST is ready"
 
 sudo mkdir -p /var/data
 sudo chown -R ubuntu:ubuntu /var/data
-#pg_dump $ClONE_DB > /var/data/smp.sql
+pg_dump > /var/data/smp.sql
