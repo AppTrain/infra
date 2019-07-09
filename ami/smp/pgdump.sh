ecc4aa6c6ff717f8384e2b459dc1e56b345366fc
Jeremiah Campbell <jeremiah@integralthread.com>
2019-07-08 21:58:27 -0400

    wip smp db build

--- ami/smp/pgdump.sh
+++ ami/smp/pgdump.sh
@@ -3,11 +3,11 @@ set -e
 echo "*****************"
 echo $PACKER_BUILD_NAME
 
-pg_isready -d $CLONE_DB
-psql -d $CLONE_DB -c "select now();"
-echo "*****************"
-echo "Database $CLONE_DB at $PGHOST is ready"
+# pg_isready -d $CLONE_DB
+# psql -d $CLONE_DB -c "select now();"
+# echo "*****************"
+# echo "Database $CLONE_DB at $PGHOST is ready"
 
 sudo mkdir -p /var/data
 sudo chown -R ubuntu:ubuntu /var/data
-pg_dump $ClONE_DB > /var/data/smp.sql
+#pg_dump $ClONE_DB > /var/data/smp.sql
