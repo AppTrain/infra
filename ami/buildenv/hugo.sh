e0e66dbd433e3ff5eac42ffd4f17832fcde697d0
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-09 15:46:44 -0400

    installs hugo into buildenv

--- ami/buildenv/hugo.sh
+++ ami/buildenv/hugo.sh
@@ -1,10 +1,17 @@
 set -e 
+DEBIAN_FRONTEND=noninteractive
+sleep 5
+mkdir ~/Downloads
+sudo apt-get update && sudo apt-get upgrade -y -q
+sudo apt-get install -y -q jq
 
-#sudo apt-get install jq
 LATEST=https://api.github.com/repos/gohugoio/hugo/releases/latest
-QUERY='.assets[] | select(.name | contains("Linux-64bit.deb") and (contains("extended") | not )) | {name: .name, url: .url}'
+QUERY='.assets[] | select(.name | contains("Linux-64bit.deb") and (contains("extended") | not )) | {name: .name, url: .browser_download_url}'
 
 meta=$( curl $LATEST | jq "$QUERY" )
 name=$(echo $meta | jq '.name' | tr -d '"')
 url=$(echo $meta | jq '.url' | tr -d '"')
-curl -o $name $url
+cd ~/Downloads 
+curl -L -o $name $url 
+dpkg-deb --info $name
+sudo dpkg -i $name
