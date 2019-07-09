1ee3f19b918212f6d54d902fbcb024f9509d2655
Jeremiah Campbell <jeremiah@integralthread.com>
2019-07-09 00:34:50 -0400

    deploying ssh keys for buildkite access to github

--- aws/accuen/ops/repos/github.tf
+++ aws/accuen/ops/repos/github.tf
@@ -2,3 +2,8 @@ provider "github" {
   organization = "accuenmedia"
   version = "2.1"
 }
+
+resource "github_user_ssh_key" "buildkite" {
+  title = "buildkite build env 2019 07"
+  key   = "${file("${pathexpand("~/.annalect/ssh/buildkite.pub")}")}"
+}
