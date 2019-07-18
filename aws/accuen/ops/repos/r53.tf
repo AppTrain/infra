a8fdccaf5a47c550b87dca44892b7eea6fd6f132
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-18 13:49:44 -0400

    aws  ecr won't let me have nice things

--- aws/accuen/ops/repos/r53.tf
+++ aws/accuen/ops/repos/r53.tf
@@ -1,18 +1,22 @@
 
+## LEAVING THIS HERE TO REMIND ME
+## THAT ECR WON'T LET ME HAVE NICE
+## DOMIAN NAMES. DAMN THEM.
+
 # could not find data source to grab just domain,
 # just setting variable for now.
-variable "registry_host" {
-    default = "348194362585.dkr.ecr.us-west-2.amazonaws.com" 
-}
+# variable "registry_host" {
+#     default = "348194362585.dkr.ecr.us-west-2.amazonaws.com" 
+# }
 
-data "aws_route53_zone" "main" {
-  name         = "accuenplatform.com."
-}
+# data "aws_route53_zone" "main" {
+#   name         = "accuenplatform.com."
+# }
 
-resource "aws_route53_record" "dkr" {
-  zone_id = "${data.aws_route53_zone.main.id}"
-  name    = "dkr.accuenplatform.com"
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${var.registry_host}"]
-}
+# resource "aws_route53_record" "dkr" {
+#   zone_id = "${data.aws_route53_zone.main.id}"
+#   name    = "dkr.accuenplatform.com"
+#   type    = "CNAME"
+#   ttl     = 300
+#   records = ["${var.registry_host}"]
+# }
