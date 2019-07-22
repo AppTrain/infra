700b8fbcdb188a99070e4eaf56d5bedffbb03ed1
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-22 15:21:33 -0400

    wip r53 name for internal builder server

---
+++ aws/accuen/ops/build/r53.tf
@@ -0,0 +1,12 @@
+data "aws_route53_zone" "outside" {
+  name         = "build-ops.out.accuenplatform.com."
+}
+
+resource "aws_route53_record" "builder" {
+  zone_id = "${data.aws_route53_zone.outside.id}"
+  name    = "b${count.index}.build-ops.out.accuenplatform.com"
+  type    = "A"
+  ttl     = 300
+  records = ["${aws_instance.builder[count.index].private_ip}"]
+  count = "${var.builder_count}"
+}
