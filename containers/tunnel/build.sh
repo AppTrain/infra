e56d930cdb7bbdab6f90812111f0a58451b56825
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-25 14:43:19 -0400

    rm notify

--- containers/tunnel/build.sh
+++ containers/tunnel/build.sh
@@ -11,8 +11,3 @@ docker build -t 348194362585.dkr.ecr.us-west-2.amazonaws.com/ops/tunnel .
 docker push 348194362585.dkr.ecr.us-west-2.amazonaws.com/ops/tunnel
 rm -rf $KEY_DIR
 
-if [ "$BUILDKITE" == "true" ]; then
-    NOTIFY_DIR=/var/lib/buildkite-agent/slack #TODO: add to env config
-    cd $NOTIFY_DIR
-    ~/.poetry/bin/poetry run python notify.py
-fi
