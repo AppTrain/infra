70c98517d6b0e6ea7d5205594d5ce9eba9e5edef
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-07-30 11:44:10 -0400

    build scripts

---
+++ aws/accuen/ops/base/build.sh
@@ -0,0 +1,5 @@
+export AWS_DEFAULT_REGION=us-west-2
+
+terraform init
+terraform plan -detailed-exitcode
+# -out=path ? add plan file to artifact?
--- containers/tunnel/build.sh
+++ containers/tunnel/build.sh
@@ -1,5 +1,5 @@
 set -e 
-
+export AWS_DEFAULT_REGION=us-west-2
 THIS_DIR=$(pwd)
 KEY_DIR=$THIS_DIR/keys
 echo "THIS DIR: $THIS_DIR"
@@ -8,6 +8,8 @@ echo "KEY DIR: $KEY_DIR"
 mkdir $KEY_DIR
 cp ~/.annalect/ssh/* keys
 docker build -t 348194362585.dkr.ecr.us-west-2.amazonaws.com/ops/tunnel .
+
+eval $(aws ecr get-login --no-include-email)
 docker push 348194362585.dkr.ecr.us-west-2.amazonaws.com/ops/tunnel
 rm -rf $KEY_DIR
 
