1d61f3ec3b5d873123c589c89ba5cc8459e8cb40
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-08-02 16:44:28 -0400

    new build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+export BUILD_CONTEXT=$(pwd)
+export AWS_DEFAULT_REGION=us-west-2
+
+echo "--- :shell: prepare build"
+echo 
+
+echo "--- use :aws: cli to :docker: login to :ecr:"
+eval $(aws ecr get-login --no-include-email)
+
+echo "--- :terraform: aws/accuen/ops/base"
+cd aws/accuen/ops/base && ./build.sh
+cd $BUILD_CONTEXT
+
+echo "--- :terraform: aws/accuen/ops/build"
+cd aws/accuen/ops/build && terraform plan -detailed-exit-code
+cd $BUILD_CONTEXT
+
+echo "--- :docker: build & push containers/tunnel"
+cd containers/tunnel && ./build.sh
+cd $BUILD_CONTEXT
+
+echo "--- :docker: build & push containers/tunnel"
+cd containers/pgcli && ./build.sh
+cd $BUILD_CONTEXT 
+
+echo "+++ the ending"
+function inline_image {
+  printf '\033]1338;url='"$1"';alt='"$2"'\a\n'
+}
+
+inline_image https://media.giphy.com/media/QMsS2IxP812wbn4WeE/giphy.gif 'I believe it is done'
