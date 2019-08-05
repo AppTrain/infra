e679991e3a371ef3e5a73acd29258b438d815ab8
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-08-05 16:19:26 -0400

    just use one airflow repo

--- aws/accuen/ops/repos/ecr.tf
+++ aws/accuen/ops/repos/ecr.tf
@@ -35,20 +35,11 @@ resource "aws_ecr_repository" "pgcli" {
   }
 }
 
-resource "aws_ecr_repository" "airflow_base" {
-  name = "airflow/base"
+resource "aws_ecr_repository" "airflow" {
+  name = "ops/airflow"
   tags = {
     Terraform = true
     github    = "https://github.com/accuenmedia/airflow"
     description = "airflow base"
-  }
-}
-
-resource "aws_ecr_repository" "airflow" {
-  name = "airflow/airflow"
-  tags = {
-    Terraform = true
-    github    = "https://github.com/accuenmedia/airflow"
-    description = "airflow"
   }
 }
