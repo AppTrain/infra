2dec846115f41c778670f26ea6da9895a96a890a
Jeremiah Campbell <jeremiah@integralthread.com>
2019-08-26 23:42:45 -0400

    dev1 wip [skip ci]

--- aws/accuen/dev1/build.sh
+++ aws/accuen/dev1/build.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+set -euo pipefail
 export AWS_DEFAULT_REGION=us-west-2
 
 terraform init
 terraform plan -detailed-exitcode
 # -out=path ? add plan file to artifact?
-#terraform apply -auto-approve
