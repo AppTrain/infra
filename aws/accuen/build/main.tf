c95da1d63f84d8fce27a9e1d379c24ef792d44f9
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2019-11-25 10:47:10 -0500

    0.12 string interpolation update

--- aws/accuen/build/main.tf
+++ aws/accuen/build/main.tf
@@ -10,7 +10,6 @@ locals {
   env = "build_ops_v2"
 }
 
-
 # provider "aws" {
 #   region = "us-west-2"
 #   version = "2.15"
@@ -18,18 +17,17 @@ locals {
 
 resource "aws_key_pair" "builder" {
   key_name   = "annalect_${local.env}_builder_v2"
-  public_key = "${file("${pathexpand("~/.annalect/ssh/builder.pub")}")}"
+  public_key = file(pathexpand("~/.annalect/ssh/builder.pub"))
 }
 
-
 #build_ops
-
 
 module "buildkite_env" {
   #source = "git@github.com:accuenmedia/buildenv.git//terraform"
   source = "../../../../buildenv/terraform"
+
   #user      = "jeremiah"
-  env           = "${local.env}"
+  env           = local.env
   instance_type = "m5.xlarge"
-  ssh_key_pair  = "${aws_key_pair.builder.key_name}"
+  ssh_key_pair  = aws_key_pair.builder.key_name
 }
