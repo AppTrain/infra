a23fa734d870b91794014cb5bfb563e7f94c75d8
Jeremiah Campbell <jeremiah.campbell@annalect.com>
2020-01-16 14:15:40 -0500

    wip entrypoint

--- terraform-modules/buildenv/main.tf
+++ terraform-modules/buildenv/main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "entrypoint" {
   key_name   = "annalect-${var.env}-entrypoint"
-  public_key = "${file("${pathexpand("~/.annalect/ssh/entrypoint.pub")}")}"
+  public_key = "${file("${pathexpand("../../etc/entrypoint.pub")}")}"
 }
