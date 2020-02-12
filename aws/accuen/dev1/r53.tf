e16837b738cd95cdf8e7c5666f3de05938faef84
Jeremiah Campbell <jeremiah@integralthread.com>
2020-02-11 22:44:07 -0500

    add internal dns zone to dev1

--- aws/accuen/dev1/r53.tf
+++ aws/accuen/dev1/r53.tf
@@ -24,3 +24,29 @@ resource "aws_route53_record" "external-ns" {
     aws_route53_zone.external.name_servers[3],
   ]
 }
+
+resource "aws_route53_zone" "internal" {
+  name = "${local.env}.in.accuenplatform.com."
+  vpc {
+    vpc_id = module.this_vpc.vpc_id
+  }
+
+  tags = {
+    zone        = "${local.env}.in.accuenplatform.com"
+    Environment = local.env
+  }
+}
+
+resource "aws_route53_record" "internal-ns" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${local.env}.in.accuenplatform.com"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    aws_route53_zone.internal.name_servers[0],
+    aws_route53_zone.internal.name_servers[1],
+    aws_route53_zone.internal.name_servers[2],
+    aws_route53_zone.internal.name_servers[3],
+  ]
+}
